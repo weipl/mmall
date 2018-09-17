3093473083429f73044894eafca93c7fa27f57d0
WPL <534973453@qq.com>
2018-09-18 01:12:28 +0800

    购物车模块开发&测试
    有个疑问-->https://coding.imooc.com/learn/questiondetail/52134.html

---
+++ src/main/java/com/mmall/vo/CartVo.java
@@ -0,0 +1,51 @@
+package com.mmall.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @ Author     ：weipenglin.
+ * @ Date       ：Created in 21:39 2018/9/17
+ * @ Description：
+ * @ Modified By：
+ * @Version:
+ */
+public class CartVo {
+
+    private List<CartProductVo> cartProductVoList;
+    private BigDecimal cartTotalPrice;
+    private Boolean allChecked;//是否已经都勾选
+    private String imageHost;
+
+    public List<CartProductVo> getCartProductVoList() {
+        return cartProductVoList;
+    }
+
+    public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
+        this.cartProductVoList = cartProductVoList;
+    }
+
+    public BigDecimal getCartTotalPrice() {
+        return cartTotalPrice;
+    }
+
+    public void setCartTotalPrice(BigDecimal cartTotalPrice) {
+        this.cartTotalPrice = cartTotalPrice;
+    }
+
+    public Boolean getAllChecked() {
+        return allChecked;
+    }
+
+    public void setAllChecked(Boolean allChecked) {
+        this.allChecked = allChecked;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
