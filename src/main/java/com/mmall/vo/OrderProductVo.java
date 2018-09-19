f769c65571a8ac00dc98967e6a6af4e1e13692c9
WPL <534973453@qq.com>
2018-09-19 18:38:18 +0800

    支付模块开发&订单管理模块开发%接口测试

---
+++ src/main/java/com/mmall/vo/OrderProductVo.java
@@ -0,0 +1,41 @@
+package com.mmall.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @ Author     ：weipenglin.
+ * @ Date       ：Created in 21:24 2018/9/18
+ * @ Description：
+ * @ Modified By：
+ * @Version:
+ */
+public class OrderProductVo {
+    private List<OrderItemVo> orderItemVoList;
+    private BigDecimal productTotalPrice;
+    private String imageHost;
+
+    public List<OrderItemVo> getOrderItemVoList() {
+        return orderItemVoList;
+    }
+
+    public void setOrderItemVoList(List<OrderItemVo> orderItemVoList) {
+        this.orderItemVoList = orderItemVoList;
+    }
+
+    public BigDecimal getProductTotalPrice() {
+        return productTotalPrice;
+    }
+
+    public void setProductTotalPrice(BigDecimal productTotalPrice) {
+        this.productTotalPrice = productTotalPrice;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
